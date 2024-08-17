be1185cb022a754cdfd105a89039498ea81561f6
LuciaLigliani <lucia.ligliani@studenti.unicam.it>
2024-08-17 13:19:46 +0000

    Implement the calculation endpoint

--- src/server/routes/calculatorRoutes.js
+++ src/server/routes/calculatorRoutes.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-router.route('/calculate').get((req, res) => {
-  res.send('Hello World!');
+
+// Function to check if a number is prime
+function isPrime(num) {
+  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+      if (num % i === 0) return false;
+  }
+  return num > 1;
+}
+
+
+// Function to find the next prime number after a given number
+function nextPrime(n) {
+  let prime = n + 1;
+  while (true) {
+      if (isPrime(prime)) return prime;
+      prime++;
+  }
+}
+
+
+// endpoint to calculate the sum of two numbers and the next prime number
+router.route('/calculate').post((req, res) => {
+  const { num1, num2 } = req.body;
+  const higherNumber = Math.max(num1, num2);
+  const prime = nextPrime(higherNumber);
+  const result = num1 + num2 + prime;
+  res.json({ result });
 })
+
 
 module.exports = router;
