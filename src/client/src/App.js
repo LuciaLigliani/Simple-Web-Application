b3d87ea804646bb0274444f3797bdeae54aab2c3
LuciaLigliani <lucia.ligliani@studenti.unicam.it>
2024-08-19 17:02:54 +0000

    Add "page not found" for any wrong URLs

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculator';
+import PageNotFound from './components/pageNotFound';
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <Calculator />
-        </div>
+        <Router>
+            <div className="App">
+                {/* All possible routes in the application */}
+                <Routes>
+                    {/* Calculator URL */}
+                    <Route path="/" element={<Calculator />} />
+                    {/* Any other URL */}
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </div>
+        </Router>    
     );
 }
 
